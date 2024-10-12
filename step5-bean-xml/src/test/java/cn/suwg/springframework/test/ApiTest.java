88e594880bdb08dd190edce248ef763548642975
suwg <suwg@xiaopeng.com>
2024-10-12 16:45:59 +0800

    feat: step5 测试获取远程文件url调整

--- step5-bean-xml/src/test/java/cn/suwg/springframework/test/ApiTest.java
+++ step5-bean-xml/src/test/java/cn/suwg/springframework/test/ApiTest.java
@@ -76,7 +76,7 @@ public class ApiTest {
 
     @Test
     public void test_url() throws IOException {
-        Resource resource = resourceLoader.getResource("https://github.com/fuzhengwei/small-spring/important.properties");
+        Resource resource = resourceLoader.getResource("https://github.com/swg209/spring-study/blob/main/step5-bean-xml/src/test/resources/important.properties");
         InputStream inputStream = resource.getInputStream();
         String content = IoUtil.readUtf8(inputStream);
         System.out.println(content);
